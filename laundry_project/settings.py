95dfe6a06400a0ce4786ffc3581b4ced8c73428d
Rishu Tiwari <rishutiwari978@gmail.com>
2026-05-02 11:23:59 +0530

    debug on

--- laundry_project/settings.py
+++ laundry_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o-kh(@86nzmyr6)e1ho7n0nx852_9_5v0xtp#5(4d$wjlq-*t9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = T
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
